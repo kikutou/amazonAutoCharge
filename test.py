606ad8454873af3eaefb4fc30cc7abaac96d0b35
kiku <juteng2005@gmail.com>
2016-07-12 14:06:12 +0900

    test of captcha solving methon success

--- test.py
+++ test.py
@@ -1,25 +1,72 @@
 # coding=utf-8
 from splinter import Browser
 from flask import session
+import time
+import os
 
 
 def test(url):
     browser = Browser('firefox')
-    if session:
-        print 'get session'
-        opened = session['opened_window']
-        # opened.close()
-        print opened
-        result = 'success'
-        print 'session cleared'
+    browser.visit(url)
+    # print browser.windows.current
+    #
+    # result = browser.windows.current
+    #
+    # return result
 
-    else:
-        print 'no session'
-        browser.visit(url)
-        result = browser.windows.current
-    print result
+
+    # print 'start circle'
+    # while True:
+    #     time.sleep(3)
+    #     print 'search session'
+        # if session:
+        #     break
+    # print 'get session'
+    # opened = session['data']
+    # opened = session['opened_window']
+    # opened.close()
+    # print opened
+    # browser.windows.current.close()
+    # result = 'success'
+
+    # if session:
+    #     print 'has session'
+    #     print session
+    #     browser.windows.current = session['opened_window']
+    #     browser.find_by_id('lst-ib').fill(url)
+    #     result = 'input find success'
+    #
+    # else:
+    #     print 'no session'
+    #     browser.visit(url)
+    #     result = browser.windows.current
+    # print result
+    result = 'input fail'
+    while True:
+        time.sleep(3)
+        fileName = 'captchaFile.txt'
+        if os.path.exists(fileName):
+            print 'txt exist'
+            result = open(fileName).read()
+            browser.find_by_id('lst-ib').fill(result)
+            break
 
     return result
+
+
+def fill(window):
+    print window
+    browser = Browser('firefox')
+    browser.windows.current = window
+    print browser.windows.current
+    print browser.find_by_id('lst-ib')
+    browser.find_by_id('lst-ib').fill('input find success')
+
+    return 'input find success'
+
+# def cercle_text():
+
+
 
 if __name__ == '__main__':
     result = test('http://www.google.co.jp/')
--- testApp.py
+++ testApp.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, session, redirect
 import pickle
-import json
+import demjson
+import codecs
+import os
 
 import test
 
@@ -14,33 +16,70 @@ app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 
 
 @app.route('/')
-
-
 def index():
     return render_template('test.html')
 
 
 @app.route('/run_test')
-
-
 def run_test():
     url = request.args.get('url')
     print url
-    result = test.test(url)
-    session['opened_window'] = pickle.dumps(result)
-    print 'input session'
-
+    # global the_window
+    # the_window = [test.test(url)]
+    # print the_window
+    #
+    the_window = test.test(url)
+    # json_window = demjson.encode(the_window)
+    # session['opened_window'] = json_window
+    # print 'input session'
+    print the_window
     return render_template('test.html')
+
+
+@app.route('/test2', methods=['get'])
+def test2():
+    return render_template('test2.html')
+
+
+@app.route('/input_session')
+def input_data():
+    put = request.args.get('session_data')
+    result = test.test(put)
+    print result
+    return render_template('test2.html')
+# def input_session():
+#     global the_window
+#     return test.fill(the_window[0])
+    # session['data'] = request.args.get('session_data')
+    # msg = 'session inputted'
+    # print msg
+    # return msg
 
 
 @app.route('/clear_session')
-
 def clear_session():
     session.clear()
     print 'session cleared'
+    print session
+    print 'txt removed'
+    f = 'captchaFile.txt'
+    if os.path.exists(f):
+        os.remove(f)
+
     return render_template('test.html')
+
+@app.route('/input_text')
+def input_text():
+    text = request.args.get('text_data')
+    print text
+    captcha_file = codecs.open('captchaFile.txt', 'w')
+    captcha_file.write(text)
+
+
+    return 'new success'
+
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=3000)
+    app.run(debug=True, threaded=True, port=3090)

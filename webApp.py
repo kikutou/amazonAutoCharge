c83cc38289fc76c2da80b6fdd1ace6beba5c8998
wangrunbo <wangrunbo921@gmail.com>
2016-08-06 22:38:23 +0900

    fix email

--- webApp.py
+++ webApp.py
@@ -188,9 +188,13 @@ def amazon_login():
 def auto_charge():
 
     email = request.form['email']
-    codes = request.form['codes']
 
     print 'charge_codes' + '---' + email
+    
+
+    codes = request.form['codes']
+
+
 
     # email = demjson.decode(email_json)
     # codes = demjson.decode(codes_json)

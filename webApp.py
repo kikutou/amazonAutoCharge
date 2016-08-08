f9010cd61eeb977650b7e097e8251cdcb38dbbb5
wangrunbo <wangrunbo921@gmail.com>
2016-08-08 11:21:17 +0900

    fixcode

--- webApp.py
+++ webApp.py
@@ -324,10 +324,10 @@ def auto_charge():
 
             print 'req'
             print response.text
-            print response.text['result']
+            # print response.text['result']
             print response
             print response.content
-            print response.content['result']
+            # print response.content['result']
 
 
 

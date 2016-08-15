f634fd5fc95149627a4f04be26f8c157136e2d5c
wangrunbo <wangrunbo921@gmail.com>
2016-08-15 13:53:13 +0900

    file for charge

--- webApp.py
+++ webApp.py
@@ -73,8 +73,8 @@ class Code(db.Model):
     code = db.Column(db.String(30), unique=True, nullable=False)
     result = db.Column(db.Integer)
     message = db.Column(db.Text)
-    balance = db.Column(db.UnicodeText, nullable=True)
-    amount = db.Column(db.UnicodeText, nullable=True)
+    balance = db.Column(db.Text, nullable=True)
+    amount = db.Column(db.Text, nullable=True)
 
     trade_id = db.Column(db.Integer, db.ForeignKey('trades.id'))
 

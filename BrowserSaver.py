4b9b8c6580828fbd6ed57dc730be29fafa43147c
wangrunbo <wangrunbo921@gmail.com>
2016-08-18 18:02:08 +0900

    fix borwserSaver

--- BrowserSaver.py
+++ BrowserSaver.py
@@ -11,12 +11,12 @@ class Browsers(object):
 
     def set_browser(self, id, browser):
 
-        for __id, __browser in self.__browsers.items():
-            if __browser['browser'] is None:
-                self.__browsers.pop(__id)
-            elif time.time() - __browser['time'] > 900:
-                __browser['browser'].quit()
-                self.__browsers.pop(__id)
+        # for __id, __browser in self.__browsers.items():
+        #     if __browser['browser'] is None:
+        #         self.__browsers.pop(__id)
+        #     elif time.time() - __browser['time'] > 900:
+        #         __browser['browser'].quit()
+        #         self.__browsers.pop(__id)
 
         self.__browsers[id] = {'browser': browser, 'time': time.time()}
 

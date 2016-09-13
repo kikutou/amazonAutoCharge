34e117eec1e832d5f9e30c20536d3cf8d335117a
wangrunbo <wangrunbo921@gmail.com>
2016-09-13 13:46:22 +0900

    admin page

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
 # coding=utf-8
-import requests
-report = {
-    'email': 'nightblizzard@sina.com',
-    'password': 'sc07051989'
-}
-response = requests.post("http://52.197.23.9:4000/amazon-login", data=report, verify=False)
+import MySQLdb
+import os
+secret_key=os.urandom(24)
+print secret_key.encode('hex')
 
-print response.status_code
+# admindb = MySQLdb.connect("localhost", "root", "sc07051989", "admin")
+#
+# cursor = admindb.cursor()
+# u = "vnsuser"
+# p = "919vnsuser32"
+# ps = "lwkeglw"
+#
+# sql = 'select * from accounts where account="%s" and password=MD5("%s")' % (u,ps)
+#
+# cursor.execute(sql)
+#
+# # 使用 fetchone() 方法获取一条数据库。
+# data = cursor.fetchone()
+#
+# print data
+#
+# # 关闭数据库连接
+# admindb.close()
+#
+# if data:
+#     print 'yes'
+# else:
+#     print 'no'
+
+

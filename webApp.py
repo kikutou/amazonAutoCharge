848abf06f56514b107c434b497a50c3b3987b509
wangrunbo <wangrunbo921@gmail.com>
2016-09-16 17:26:44 +0900

    https

--- webApp.py
+++ webApp.py
@@ -804,7 +804,7 @@ def getReq():
 
         os.environ['DISPLAY'] = ':1'
 
-        browser = Browser('firefox', profile='/home/ubuntu/.mozilla/firefox/')
+        browser = Browser('firefox', profile='/home/ubuntu/.mozilla/firefox/profiles.ini')
         browser.visit('https://54.238.194.108/')
 
         return 'get data='+email+'and'+password

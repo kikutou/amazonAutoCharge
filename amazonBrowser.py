85c505110bc86c29bc4e396e30a1e65ef62785b9
wangrunbo <wangrunbo921@gmail.com>
2016-09-30 16:36:01 +0900

    message

--- amazonBrowser.py
+++ amazonBrowser.py
@@ -327,7 +327,7 @@ def amazon_charge(browser, code):
             return {
                 'code': 3,
                 'title': '番号違いエラー',
-                'message': '番号違いでエラーが発生しました。'.encode('utf-8'),
+                'message': u'番号違いでエラーが発生しました。',
                 'html_code_before_charge': html_code_before_charge,
                 'html_code_after_charge': html_code_after_charge
             }
